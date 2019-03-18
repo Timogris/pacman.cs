e0284abd33dc57896d2454daa2712fbc3de1bd9d
Ariana La toute puissante <ariana@hubrix.fr>
2019-03-18 17:56:16 +0100

    Nodes
    half of the nodes are ok for script

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
+    public Node[] neighbors;
+    public Vector2[] validDirections;
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
